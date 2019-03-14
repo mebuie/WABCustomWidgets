f5855fb6ca003e860f728bd255dafad616f7a545
mbuie <mbuie@cityofmesquite.com>
2019-03-14 09:49:30 -0500

    Added i18n support

--- widgets/MyDemo/nls/strings.js
+++ widgets/MyDemo/nls/strings.js
@@ -1,9 +1,7 @@
 define({
-  root: ({
-    _widgetLabel: "Demo",
-    _featureAction_ShowVertex: "Show Vertex Count",
+  root: {
     label1: "I am a demo widget.",
     label2: "This is configurable."
-  }),
-  "es": 0
+  },
+  "es": 1
 });
