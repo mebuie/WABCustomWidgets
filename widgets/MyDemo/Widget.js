d08ed19de5e152934a393ae01e3ba554d6c70efc
mbuie <mbuie@cityofmesquite.com>
2019-03-14 09:28:42 -0500

    Added note about need for including _WidgetsInTemplateMixin.

--- widgets/MyDemo/Widget.js
+++ widgets/MyDemo/Widget.js
@@ -2,6 +2,11 @@ define(['dojo/_base/declare', 'jimu/BaseWidget'],
 function(declare, BaseWidget) {
   //To create a widget, you need to derive from BaseWidget.
   return declare([BaseWidget], {
+
+    // Note, the widget's template does not support dijits in the template. If you need to use dijits in the
+    // template, add dijit/_WidgetsInTemplateMixin to your required list, and ass this class to your widget.
+    // See define the template section of documentation. 
+
     // DemoWidget code goes here
 
     //please note that this property is be set by the framework when widget is loaded.
