0946e33143a54a7f3371bcdbfd518962e0fe1e65
mbuie <mbuie@cityofmesquite.com>
2019-03-14 11:27:09 -0500

    Added title and tip

--- widgets/MyDemo/setting/nls/strings.js
+++ widgets/MyDemo/setting/nls/strings.js
@@ -1,5 +1,7 @@
 define({
 	root: ({
-		configText: "Set config text:"
+		configText: "Set config text:",
+    settingsTitle: "Multi Filter Settings",
+    settingsTip: "Add one or more query group to the widget and configure parameters for each of them. "
   })
 });
