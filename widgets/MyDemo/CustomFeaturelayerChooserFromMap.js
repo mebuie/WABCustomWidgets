18c769cefa071bb4269662b7bbd550a1aba66b70
mbuie <mbuie@cityofmesquite.com>
2019-03-15 16:33:56 -0500

    Updated license.

--- widgets/MyDemo/CustomFeaturelayerChooserFromMap.js
+++ widgets/MyDemo/CustomFeaturelayerChooserFromMap.js
@@ -14,7 +14,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// This file has been modified from the Filter widget by Mark Buie to add support
+// for group filtering.
 ///////////////////////////////////////////////////////////////////////////
+
 
 define([
   'dojo/_base/declare',
