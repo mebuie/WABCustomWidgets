2250fa33107b8e18e1fe3d8bf5e2d3baa1792547
mbuie <mbuie@cityofmesquite.com>
2019-03-18 13:44:24 -0500

    updated strings

--- widgets/MyDemo/setting/nls/strings.js
+++ widgets/MyDemo/setting/nls/strings.js
@@ -3,6 +3,8 @@ define({
 		configText: "Set config text:",
     settingsTitle: "Multi Filter Settings",
     settingsTip: "Add one or more query groups to the widget and configure parameters for each of them.",
-    newFilter: "New Group Filter"
+    newFilter: "New Group Filter",
+    groupName: "Filter Group Name",
+    groupNameValidationBoxHint: "Give your group a name"
   })
 });
