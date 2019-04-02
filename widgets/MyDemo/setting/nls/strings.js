856f4a5ce84af9a467a8aa11774f2c97797be34b
mbuie <mbuie@cityofmesquite.com>
2019-04-02 11:38:44 -0500

    Updated newFilter string.

--- widgets/MyDemo/setting/nls/strings.js
+++ widgets/MyDemo/setting/nls/strings.js
@@ -4,7 +4,7 @@ define({
     settingsTitle: "Multi Filter Settings",
     settingsTip: "Add one or more query groups to the widget and configure parameters for each of them.",
     newGroup: "New Group Filter",
-    newFilter: "New Filter",
+    newFilter: "New Layer",
     groupName: "Filter Group Name",
     groupNameValidationBoxHint: "Give your group a name",
     layer: "Layer"
