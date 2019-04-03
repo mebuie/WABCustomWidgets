05570cea1bed4bab3ce6adba7339b037a5f9e04f
mbuie <mbuie@cityofmesquite.com>
2019-04-03 10:27:16 -0500

    Base template.

--- widgets/MyDemo/Widget.js
+++ widgets/MyDemo/Widget.js
@@ -19,20 +19,16 @@
 // for group filtering.
 ///////////////////////////////////////////////////////////////////////////
 
-define(['dojo/_base/declare', 'jimu/BaseWidget'],
-function(declare, BaseWidget) {
-  //To create a widget, you need to derive from BaseWidget.
-  return declare([BaseWidget], {
+define([
+  'dijit/_WidgetsInTemplateMixin',
+  'dojo/_base/declare',
+  'jimu/BaseWidget'],
+function(
+  _WidgetsInTemplateMixin,
+  declare,
+  BaseWidget) {
 
-    // Note, the widget's template does not support dijits in the template. If you need to use dijits in the
-    // template, add dijit/_WidgetsInTemplateMixin to your required list, and ass this class to your widget.
-    // See define the template section of documentation.
-
-    // DemoWidget code goes here
-
-    //please note that this property is be set by the framework when widget is loaded.
-    //templateString: template,
-
+  return declare([_WidgetsInTemplateMixin, BaseWidget], {
     baseClass: 'jimu-widget-mydemo',
 
     postCreate: function() {
@@ -41,8 +37,6 @@ function(declare, BaseWidget) {
     },
 
     startup: function() {
-      this.inherited(arguments);
-      this.mapIdNode.innerHTML = 'map id:' + this.map.id;
       console.log('startup');
     },
 
@@ -69,10 +63,7 @@ function(declare, BaseWidget) {
 
     onSignOut: function(){
       console.log('onSignOut');
-    },
-
-    showVertexCount: function(count){
-      this.vertexCount.innerHTML = 'The vertex count is: ' + count;
     }
+
   });
 });
